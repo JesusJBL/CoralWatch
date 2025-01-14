8ef08aab4e262d063eaabd62822ebdf220160e35
Jesus Bautista Lopez <jesusjblopez@gmail.com>
2025-01-14 18:01:15 -0400

    Add changes

--- server/aws.py
+++
@@ -1,5 +0,0 @@
-import boto3
-
-
-s3 = boto3.client('s3')
-s3.download_file('coralmodelbucket', 'model6.keras', 'model6.keras')
--- server/model.py
+++ server/model.py
@@ -4,12 +4,18 @@ import json
 import tensorflow as tf
 from tensorflow  import keras
 import numpy as np
+import boto3
 
 
 
 class Model:
     def __init__(self):
+        self.download_model_from_s3('coralmodelbucket', 'model6.keras')
         self.model = tf.keras.models.load_model('./model6.keras')
+    
+    def download_model_from_s3(self, bucket_name, model_key):
+        s3 = boto3.client('s3')
+        s3.download_file(bucket_name, model_key, 'model6.keras')
     
     def prepareImage(self, picture):
         imageTransformed = tf.keras.utils.load_img(picture, target_size=(160,160), color_mode="rgb", keep_aspect_ratio=False)
